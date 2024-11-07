657591da002164d0726f17d6521d44853012e1e1
Huong <huongmtgcd201620@fpt.edu.vn>
2024-11-07 23:16:34 +0700

    Class and Course Navigation

--- app/src/main/java/com/example/coursework_1786/MainActivity.java
+++ app/src/main/java/com/example/coursework_1786/MainActivity.java
@@ -1,24 +1,61 @@
 package com.example.coursework_1786;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.coursework_1786.Fragments.YogaClassFragment;
+import com.example.coursework_1786.Fragments.YogaCourseFragment;
+import com.example.coursework_1786.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+    Button createCourseBtn;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.labelTimeOfCourse), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        replaceFragment(new YogaCourseFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item ->{
+            switch (item.getItemId()){
+                case R.id.yogaCourse:
+                    replaceFragment(new YogaCourseFragment());
+                    break;
+                case R.id.yogaClass:
+                    replaceFragment(new YogaClassFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
